f8390992c2879337ed9ffc88ba218bda643e2887
Haris Umar <haris29268@gmail.com>
2023-05-31 01:12:44 +0500

    Modified CORS Policy

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const cors = require("cors");
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(cors());
 
